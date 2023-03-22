c03727568b29b5fd4f69c0e49c5214889b94fb15
Luis Caro <luis.carosalgado1@gmail.com>
2023-03-22 20:16:10 -0300

    prueba2

--- src/components/Narv.js
+++ src/components/Narv.js
@@ -41,8 +41,8 @@ function Narv()
         </li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="Busca Tu servicio" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
