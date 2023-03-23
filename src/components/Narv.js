9a047d1e469197195abbc2dcf3e73f04896134a9
Felipe Ojeda <fojedai47@gmail.com>
2023-03-22 23:32:42 -0300

    login/signup view (styles missing)

--- src/components/Narv.js
+++ src/components/Narv.js
@@ -1,53 +1,62 @@
-
+import { Link, useHistory } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faHammer} from "@fortawesome/free-solid-svg-icons"
+import { faHammer } from "@fortawesome/free-solid-svg-icons";
 
+function Narv() {
+  const tool = <FontAwesomeIcon icon={faHammer} bounce />;
 
-function Narv()
-{
-    const tool = <FontAwesomeIcon icon={faHammer}  bounce />
-    return(
+  function handleClick() {
+    window.location.href = "/new-url";
+  }
 
-<nav class="navbar navbar-expand-lg bg-secondary">
-  <div class="container-fluid">
-    <h1 class="navbar-brand" style ={{width:300}}href="#"> A tu Servicio{tool}</h1>
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Registrate
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  return (
+    <nav className="navbar navbar-expand-lg bg-secondary">
+      <div className="container-fluid">
+      <Link to="/" className="navbar-brand" style={{ width: 300 }} onClick={handleClick}>
+      A tu Servicio{tool}
+    </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+            <li className="nav-item me-4">
+              <a className="nav-link active" aria-current="page" href="#">
+                Post a service
+              </a>
+            </li>
+            <li className="nav-item dropdown me-4">
+              <a
+                className="nav-link"
+                href="#"
+                role="button"
+                aria-expanded="false"
+              >
+                Find a Service
+              </a>
+            </li>
+            <li className="nav-item me-4">
+              <a className="nav-link">Login/Sign up</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Profile</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Busca Tu servicio" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Buscar</button>
-      </form>
-    </div>
-  </div>
-</nav>
-)
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Narv
+export default Narv;
--- src/views/Register-login.js
+++ src/views/Register-login.js
@@ -1,8 +1,85 @@
+import React, { useState } from 'react';
+import "../App.css";
 
 
-const Signup = () =>{
+function Signup() {
+    const [login, setLogin] = useState(true);
+
+    function handleLoginClick() {
+        setLogin(true);
+    }
+
+    function handleSignupClick() {
+        setLogin(false);
+    }
+
     return (
-        <h1>Signup</h1>
-    )
+        <div style={{
+            textAlign: 'center',
+            marginTop: '50px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}>
+            <button
+                onClick={handleLoginClick}
+                disabled={login}
+                style={{
+                    marginRight: '20px',
+                    backgroundColor: login ? '#ccc' : '#fff',
+                    padding: '10px 20px',
+                    border: 'none',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                }}
+            >
+                Login
+            </button>
+            <button
+                onClick={handleSignupClick}
+                disabled={!login}
+                style={{
+                    backgroundColor: !login ? '#ccc' : '#fff',
+                    padding: '10px 20px',
+                    border: 'none',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                }}
+            >
+                Sign up
+            </button>  {login ? (
+                <form style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Username:</label>
+                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Password:</label>
+                    <input type="password" style={{ marginBottom: '20px', padding: '5px' }} />
+                </form>
+
+            ) : (
+                <form style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Name:</label>
+                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Last Name:</label>
+                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Username:</label>
+                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Email:</label>
+                    <input type="email" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Password:</label>
+                    <input type="password" style={{ marginBottom: '20px', padding: '5px' }} />
+
+                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Profile Picture:</label>
+                    <input type="file" accept="image/*" style={{ marginBottom: '20px' }} />
+                </form>
+            )}
+            <button>Submit</button>
+        </div>
+    );
 }
+
 export default Signup;
