86c21e0426bb82c99a2f7221d6c02e850e1b5e6e
Luis-Pablo <pabloarias024@gmail.com>
2023-03-28 23:10:50 +0000

    feed en el home

--- src/views/FeedJob.js
+++ src/views/FeedJob.js
@@ -1,12 +1,14 @@
 import Feeds from "../components/Feeds";
 import SearchFeed from "../components/SearchFeed";
 
+
 const FeedJob = () => {
     return (
         <>
         
     <div className="container">
             <div className="row ">
+                <h1>Feed</h1>
                 <div className="col w-25"><SearchFeed/></div>
             
                 <div className="col w-75"><Feeds /></div>
