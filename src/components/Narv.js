9d515a1c917bba11c62ef0d0b05989601a7ff8c1
Felipe Ojeda <fojedai47@gmail.com>
2023-03-28 12:24:37 -0300

    navbar

--- src/components/Narv.js
+++ src/components/Narv.js
@@ -1,92 +1,39 @@
-<<<<<<< HEAD
-import { Link, useHistory } from "react-router-dom";
-=======
 import "../App.css";
->>>>>>> 937ffbaf3f8985b22449b0a2eaa99d82fa1b7751
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHammer } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
-
-function Narv() {
-  const tool = <FontAwesomeIcon icon={faHammer} bounce />;
-
-  function handleClick() {
-    window.location.href = "/new-url";
-  }
-
-  return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
-      <div className="container-fluid">
-      <Link to="/" className="navbar-brand" style={{ width: 300 }} onClick={handleClick}>
-      A tu Servicio{tool}
-    </Link>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-            <li className="nav-item me-4">
-              <a className="nav-link active" aria-current="page" href="#">
-                Post a service
-              </a>
-            </li>
-            <li className="nav-item dropdown me-4">
-              <a
-                className="nav-link"
-                href="#"
-                role="button"
-                aria-expanded="false"
-              >
-                Find a Service
-              </a>
-            </li>
-            <li className="nav-item me-4">
-              <a className="nav-link">Login/Sign up</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link">Profile</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-=======
+import { faHammer, faUser, faAddressCard, faMagnifyingGlass, faUpload} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 function Narv() {
   const tool = <FontAwesomeIcon icon={faHammer} bounce />;
+  const profile = <FontAwesomeIcon icon={faUser} />
+  const register = <FontAwesomeIcon icon={faAddressCard} />
+  const search = <FontAwesomeIcon icon={faMagnifyingGlass} />
+  const post = <FontAwesomeIcon icon={faUpload} />
   return (
-    
+
     <ul className="nav  nav-tabs">
-      <h3 className="ms-3">{tool} ATúservicio</h3>
-      <form className="d-flex " role="search">
-        <input className="form-control me-2 ms-5" type="search" placeholder="Search" style={{width:600}}/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-     
-    <li className="nav-item ms-auto mt-3">
-      <Link to="/" >Home</Link>
-    </li>
-    <li className="nav-item ms-3 mt-3">
-      <Link to="/signup">Register</Link>
-    </li>
-    <li className="nav-item ms-3 mt-3">
-      <Link to="profile" >Profile</Link>
-    </li>
- 
-  </ul>
->>>>>>> 937ffbaf3f8985b22449b0a2eaa99d82fa1b7751
+      <div>
+        <li className="ms-3 home bg-light rounded-pill px-5 mt-3">
+          <h3><Link to="/" >{tool} ATúservicio</Link></h3>
+        </li>
+      </div>
+      <li className="nav-item mt-3 bg-warning rounded-pill px-3">
+        <Link to="/signup">Register&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{register}</Link>
+      </li>
+
+      <li className="nav-item mt-3 bg-danger rounded-pill px-3">
+      <Link to="/profile">Profile&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{profile}</Link>
+
+      </li>
+      <li className="nav-item mt-3 bg-primary rounded-pill px-3">
+        <Link to="/service" >Find your service&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{search}</Link>
+      </li>
+      <li className="nav-item mt-3 bg-success rounded-pill px-4 me-5">
+        <Link to="/detail" >Post your service&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{post}</Link>
+      </li>
+    </ul>
   );
 }
 
--- src/views/Register-login.js
+++ src/views/Register-login.js
@@ -1,92 +1,18 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import "../App.css";
-
-=======
 import Register from "../components/Register"
 import Login from  "../components/Login"
->>>>>>> 937ffbaf3f8985b22449b0a2eaa99d82fa1b7751
 
 function Signup() {
-    const [login, setLogin] = useState(true);
+    /* const [login, setLogin] = useState(true); */
 
-    function handleLoginClick() {
-        setLogin(true);
-    }
+    // function handleLoginClick() {
+    //     setLogin(true);
+    // }
 
-    function handleSignupClick() {
-        setLogin(false);
-    }
+    // function handleSignupClick() {
+    //     setLogin(false);
+    // }
 
     return (
-<<<<<<< HEAD
-        <div style={{
-            textAlign: 'center',
-            marginTop: '50px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}>
-            <button
-                onClick={handleLoginClick}
-                disabled={login}
-                style={{
-                    marginRight: '20px',
-                    backgroundColor: login ? '#ccc' : '#fff',
-                    padding: '10px 20px',
-                    border: 'none',
-                    borderRadius: '5px',
-                    cursor: 'pointer',
-                }}
-            >
-                Login
-            </button>
-            <button
-                onClick={handleSignupClick}
-                disabled={!login}
-                style={{
-                    backgroundColor: !login ? '#ccc' : '#fff',
-                    padding: '10px 20px',
-                    border: 'none',
-                    borderRadius: '5px',
-                    cursor: 'pointer',
-                }}
-            >
-                Sign up
-            </button>  {login ? (
-                <form style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Username:</label>
-                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Password:</label>
-                    <input type="password" style={{ marginBottom: '20px', padding: '5px' }} />
-                </form>
-
-            ) : (
-                <form style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Name:</label>
-                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Last Name:</label>
-                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Username:</label>
-                    <input type="text" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Email:</label>
-                    <input type="email" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Password:</label>
-                    <input type="password" style={{ marginBottom: '20px', padding: '5px' }} />
-
-                    <label style={{ textAlign: 'center', marginBottom: '10px' }}>Profile Picture:</label>
-                    <input type="file" accept="image/*" style={{ marginBottom: '20px' }} />
-                </form>
-            )}
-            <button>Submit</button>
-        </div>
-    );
-=======
         <div className="m-3">
             <ul className="nav nav-pills mt-5 d-flex justify-content-center bg-transparent" id="pills-tab" role="tablist">
   <li className="nav-item" role="presentation">
@@ -111,7 +37,6 @@ function Signup() {
 </div>
         </div>
     )
->>>>>>> 937ffbaf3f8985b22449b0a2eaa99d82fa1b7751
 }
 
 export default Signup;
