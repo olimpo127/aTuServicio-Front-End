9371a4412ee95e62895d5cef1f43e3a3e9d90605
raul araque <luis.raul.araque2@gmail.com>
2023-04-11 20:37:52 -0500

    componentes para upload de imagenes

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { Context } from "../store/context"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUpload } from "@fortawesome/free-solid-svg-icons";
-import Avatar from "../components/Avatar";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import ImageService from "./ImageService";
 
 
 const Register = () => {
@@ -12,9 +12,9 @@ const Register = () => {
     const { store, actions } = useContext(Context);
     return (
         <div>
-            <div className="container text-center col-3 mt-5">
-                <div className="input-group m-3">
-                    <span className="input-group-text" id="basic-addon1">Name</span>
+            <div className="container text-center col-3 mt-5 was-validated">
+                <div className="input-group mb-3">
+                    <span className="input-group-text has-validation" id="basic-addon1">Name</span>
                     <input
                         type="text"
                         name="name"
@@ -24,9 +24,10 @@ const Register = () => {
                         aria-describedby="basic-addon1"
                         onChange={actions.handleChange}
                         value={store.user.name}
+                        required
                     />
                 </div>
-                <div className="input-group m-3">
+                <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon2">Lastname</span>
                     <input
                         type="text"
@@ -37,9 +38,10 @@ const Register = () => {
                         aria-describedby="basic-addon2"
                         onChange={actions.handleChange}
                         value={store.user.lastname}
+                        required
                     />
                 </div>
-                <div className="input-group m-3">
+                <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon3">Username</span>
                     <input
                         type="text"
@@ -50,11 +52,12 @@ const Register = () => {
                         aria-describedby="basic-addon3"
                         onChange={actions.handleChange}
                         value={store.user.username}
+                        required
                     />
                 </div>
-                <div className="input-group m-3">
+                <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon4">E-mail</span>
-                    <input nn 
+                    <input nn
                         type="text"
                         name="email"
                         className="form-control"
@@ -63,25 +66,31 @@ const Register = () => {
                         aria-describedby="basic-addon4"
                         onChange={actions.handleChange}
                         value={store.user.email}
+                        required
                     />
                 </div>
-                <div className=" input-group m-3">
-                    <label htmlFor="Password" className="input-group-text">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        className="form-control"
-                        id="Password"
-                        onChange={actions.handleChange}
-                        value={store.user.password}
-                    />
+                <div>
+                    <div className="mb-5">
+                        <div className=" input-group">
+                            <label htmlFor="Password" className="input-group-text">Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                id="Password"
+                                onChange={actions.handleChange}
+                                value={store.user.password}
+                                maxLength={8}
+                                required
+                            />
+                        </div>
+                        <div className="form-text">Max 8 characters</div>
+                    </div>
                 </div>
-                <Avatar/>
-
-
                 <button type="submit" className="btn btn-secondary" onClick={() => actions.handleUserRegister()}>
-                    SUBMIT
+                    SUBMIT {upload}
                 </button>
+                <ImageService/>
             </div>
         </div>
     )
